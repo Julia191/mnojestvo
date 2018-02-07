5a75372d1813bb925434f8afa021b10e0598e256
Julia191 <Psihopadka666@yandex.ru>
2018-02-07 16:20:42 +0300

    1233а

--- Construction.py
+++ Construction.py
@@ -81,36 +81,83 @@
 #     S = a*b;
 #     print(S);
 #  задача 4
-a = int(input());
-b = int(input());
-c = int(input());
-if (a < b and a < c) or (b > a and  b > c):
-    print(b);
-    print(a);
-    print(c);
-
-elif (b < a and b < c) or (c > a and  c > b):
-    print(c);
-    print(b);
-    print(a);
-elif (b < a and b < c) or (a > b and  a > c):
-    print(a);
-    print(b);
-    print(c);
-elif (c < a and c < b) or (a > c and  a > b):
-    print(a);
-    print(c);
-    print(b);
-elif (c < a and c < b) or (b > c and  b > a):
-    print(b);
-    print(c);
-    print(a);
-elif (a < b and a < c) or (c > a and c > b):
-        print(c);
-        print(a);
-        print(b);
-elif (a < b and a < c) or (c > a and c > b):
-        print(c);
-        print(a);
-        print(b);
-        # Задача 5ппппккккк
+# a = int(input())
+# b = int(input())
+# c = int(input())
+#
+#
+# if (a <= b and a <= c) and (b >= a and b >= c):
+#     print(b)
+#     print(a)
+#     print(c)
+# elif (a <= b and a <= c) and (c >= a and c >= b):
+#     print(c)
+#     print(a)
+#     print(b)
+# elif (b <= a and b <= c) and (c >= a and c >= b):
+#     print(c)
+#     print(b)
+#     print(a)
+# elif (b <= a and b <= c) and (a >= b and a >= c):
+#     print(a)
+#     print(b)
+#     print(c)
+# elif (c <= a and c <= b) and (a >= c and a >= b):
+#     print(a)
+#     print(c)
+#     print(b)
+# elif (c <= a and c <= b) and (b >= c and b >= a):
+#     print(b)
+#     print(c)
+#     print(a)
+#
+#
+# a = int(input())
+# b = int(input())
+# c = int(input())
+#
+# if a > b > c:
+#     print(a)
+#     print(c)
+#     print(b)
+#
+# if a > c > b:
+#     print(a)
+#     print(b)
+#     print(c)
+#
+# if b > a > c:
+#     print(b)
+#     print(c)
+#     print(a)
+#
+# if b > c > a:
+#     print(b)
+#     print(a)
+#     print(c)
+#
+# if c > a > b:
+#     print(c)
+#     print(b)
+#     print(a)
+#
+# if c > b > a:
+#     print(c)
+#     print(a)
+#     print(b)
+#  Задача 6
+# a = int(input())
+# if (a% 100!=11) and (a%10==1):
+#     print (a, 'программист');
+# elif ((not a % 100 == 12) and  (a% 100!=13) and  (a%100!=14)) and ((a%10==2) or (a%10==3) or (a%10==4)):
+#     print(a, 'программиста');
+# else:
+#     print(a,'программистов')
+# Задача 7
+a = input();
+sum1 = int(a[0])+int(a[1])+int(a[2]);
+sum2= int(a[3])+int(a[4])+int(a[5]);
+if sum1==sum2:
+    print('Счастливый');
+else:
+    print('Обычный');
--- b.py
+++ b.py
@@ -1,3 +1,5 @@
 # bool
-a = int(input());
-print( a > 0)
+# a = int(input());
+# print( a > 0)
+a = 42 / (4 + (2 * (-2)))
+print(a);
